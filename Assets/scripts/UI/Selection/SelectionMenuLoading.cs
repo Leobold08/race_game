69680224c2e6d33e08f4548ba462595882f06c11
ThatOneGuyC <thatoneguyextra@gmail.com>
2026-03-15 21:28:10 +0200

    pieni rewrite load textien jutuille

--- Assets/scripts/UI/Selection/SelectionMenuLoading.cs
+++ Assets/scripts/UI/Selection/SelectionMenuLoading.cs
@@ -3,105 +3,73 @@ using UnityEngine.UI;
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using System;
-
-[System.Serializable]
-public class specialTextChances
-{
-    public float error;
-    public float allthetexts;
-    public float chance;
-    public float outoftime;
-    public float juud7;
-    public float grass;
-    public float reallyspecial;
-    public float nine_trillion;
-}
+using System.Linq;
 
 public class SelectionMenuLoading : MonoBehaviour
 {
     [SerializeField] private Text loadText_text;
     private TextAsset loadTexts;
-    private Dictionary<string, string[]> textData;
-    private int index = -1;
+    private TextAsset specialLoadChances;
+    private Dictionary<string, string[]> loadTextsData;
+    private Dictionary<string, float> specialLoadChancesData;
 
     void OnEnable()
     {
         loadTexts = Resources.Load<TextAsset>("loading/loadTexts");
-        textData = JsonConvert.DeserializeObject<Dictionary<string, string[]>>(loadTexts.text);
+        loadTextsData = JsonConvert.DeserializeObject<Dictionary<string, string[]>>(loadTexts.text);
+        specialLoadChances = Resources.Load<TextAsset>("loading/specialTextChances");
+        specialLoadChancesData = JsonConvert.DeserializeObject<Dictionary<string, float>>(specialLoadChances.text);
     }
     void Start()
     {
-        // pitäs toimia
-        loadingTexts();
-        specialLoadingTexts();
+        LoadingTexts();
+        SpecialLoadingTexts();
     }
 
-    public void loadingTexts()
+    public void LoadingTexts()
     {
         UnityEngine.Random.InitState(DateTime.Now.Millisecond);
         int chance = UnityEngine.Random.Range(1, 101);
         string loadTextRarity;
         int randomIndex;
 
-        if (chance <= 2) //2%
-        {
-            loadTextRarity = "obscure";
-        }
-        else if (chance <= 8) //6%
-        {
-            loadTextRarity = "rare";
-        }
-        else if (chance <= 26) //18%
-        {
-            loadTextRarity = "uncommon";
-        }
-        else //74%
-        {
-            loadTextRarity = "general";
-        }
+        if (chance <= 2) loadTextRarity = "obscure";
+        else if (chance <= 8) loadTextRarity = "rare";
+        else if (chance <= 26) loadTextRarity = "uncommon";
+        else loadTextRarity = "general";
 
         string[] texts = loadTextRarity switch
         {
-            "general" => textData["general"],
-            "uncommon" => textData["uncommon"],
-            "rare" => textData["rare"],
-            "obscure" => textData["obscure"],
+            "general" => loadTextsData["general"],
+            "uncommon" => loadTextsData["uncommon"],
+            "rare" => loadTextsData["rare"],
+            "obscure" => loadTextsData["obscure"],
             _ => new string[] { "THIS IS NOT SUPPOSED TO SHOW UP" },
         };
+        Debug.Log($"{loadTextRarity}, {texts.Length}");
         randomIndex = UnityEngine.Random.Range(0, texts.Length);
         loadText_text.text = texts[randomIndex];
     }
 
-    public void specialLoadingTexts()
+    public void SpecialLoadingTexts()
     {
-        TextAsset specialTextChancesFile = Resources.Load<TextAsset>("loading/specialTextChances");
-        specialTextChances specialChances = JsonUtility.FromJson<specialTextChances>(specialTextChancesFile.text);
-
         UnityEngine.Random.InitState(DateTime.Now.Millisecond);
-
-        var fields = typeof(specialTextChances).GetFields();
-        foreach (var field in fields)
+        foreach (var (entry, index) in specialLoadChancesData.Select((entry, index) => (entry, index)))
         {
-            index += 1;
+            string key = entry.Key;
+            float value = entry.Value;
+            float chance = UnityEngine.Random.Range(value * 1000, 100000);
 
-            string key = field.Name;
-            float value = (float)field.GetValue(specialChances);
-
-            float sChance = UnityEngine.Random.Range(value * 1000, 100000);
-            if (sChance <= value * 1000)
+            if (chance <= value * 1000)
             {
-                loadText_text.text = textData["special"][index];
-                Debug.Log(key);
-
-                loadtextbehaviour loadtextbehaviour = gameObject.GetComponent<loadtextbehaviour>();
+                loadText_text.text = loadTextsData["special"][index];
+                loadtextbehaviour loadtextbehaviour = GetComponent<loadtextbehaviour>();
                 loadtextbehaviour.SetSpecialLoadTextBehaviour(key);
-
+                
+                Debug.Log($"{key} was triggered");
                 break;
             }
-            else
-            {
-                Debug.Log("special load text not triggered");
-            }
+            else Debug.Log("special load text not triggered");
         }
     }
 }
